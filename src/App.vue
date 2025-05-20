f9f1fdcce9e336153c50c5d4fb9f91d1946b7c18
Marcel Stadelmann <m@marceli.to>
2025-05-20 11:31:06 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="min-h-screen flex items-center justify-center">
-    <h1 class="text-4xl font-bold">Hello World</h1>
+    <h1 class="font-gt-alpina-bold text-3xl">Hello World</h1>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,47 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+        'blush': '#fbdfe9',
+        'crimson': '#d22837',
+        'evergreen': '#0a372d',
+      },
+
+      textUnderlineOffset: {
+        '5': '5px',
+      },
+
+      fontFamily: {
+        'gt-alpina-medium': ['GT-Alpina-Standard-Medium', ...defaultTheme.fontFamily.sans],
+        'gt-alpina-bold': ['GT-Alpina-Standard-Bold', ...defaultTheme.fontFamily.sans],
+      },
+
+      fontSize: {
+        'sm': '1.063rem',  // 17px
+        'md': '1.1875rem', // 19px
+        'lg': '1.313rem',  // 21px
+        'xl': '1.625rem',  // 26px
+        '2xl': '2rem',     // 32px
+        '3xl': '2.5rem',   // 40px
+      },
+
+      zIndex: {
+        '60': 60,
+        '70': 70,
+        '80': 80,
+        '90': 90,
+        '100': 100,
+      },
+
+    },
   },
   plugins: []
 }
