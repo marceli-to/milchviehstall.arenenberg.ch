6e119652fe42cbbe91d7114bda5a83fa6d31ca75
Marcel Stadelmann <m@marceli.to>
2025-06-04 09:17:57 +0200

    wip

--- src/js/components/Map.vue
+++ src/js/components/Map.vue
@@ -4,17 +4,9 @@
     <img :src="`/img/map-digitaler-kuhstall-text-${currentLang}.png`" width="1319" height="714" alt="" class="block w-full h-auto absolute top-0 left-0">
   </figure>
 </template>
-
 <script setup>
 import { useLanguageStore } from '@/js/stores/language'
 import { computed, onMounted } from 'vue'
-
 const languageStore = useLanguageStore()
-
-// Use computed to reactively track the current language
 const currentLang = computed(() => languageStore.current)
-
-onMounted(() => {
-  console.log('Current language:', currentLang)
-})
 </script>
--- src/js/components/TimeLine.vue
+++ src/js/components/TimeLine.vue
@@ -5,20 +5,24 @@
       <!-- Full vertical line -->
       <div class="absolute top-0 bottom-0 w-[2px] bg-evergreen"></div>
 
-      <!-- Time dots + labels -->
+      <!-- Steps + labels -->
+
+      <!-- Start step -->
       <div class="absolute flex flex-col items-center top-0">
         <span 
           class="text-xs absolute left-13 top-0">
           06.00
         </span>
       </div>
+      <!-- // Start step -->
 
+      <!-- Steps -->
       <div
         v-for="(step, i) in steps"
         :key="i"
         class="absolute flex flex-col items-center"
-        :style="{ top: `${step.position}%` }"
-       >
+        @click="goToStep(i)"
+        :style="{ top: `${step.position}%` }">
         <span 
           class="text-xs absolute left-20"
           v-if="i != activeIndex">
@@ -26,30 +30,34 @@
         </span>
         <div class="w-14 h-14 rounded-full bg-evergreen border-2 border-blush"></div>
       </div>
+      <!-- // Steps -->
 
+      <!-- End step -->
       <div class="absolute flex flex-col items-center bottom-0">
         <span 
           class="text-xs absolute left-13 bottom-0">
           22.00
         </span>
       </div>
+      <!-- // End step -->
 
-      <!-- Active marker + triangles -->
+      <!-- Active step + triangles -->
       <div
         class="absolute left-1/2 -translate-x-1/2 -translate-y-28 flex flex-col items-center transition-all duration-500"
-        :style="{ top: `${steps[activeIndex].position}%` }">
-        <button @click="goUp" class="text-crimson">
+        :style="steps[activeIndex] ? { top: `${steps[activeIndex].position}%` } : {}">
+        <button @click="goToPreviousStep" class="text-crimson">
           <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-34 h-auto">
             <path d="M11.748 1.97198L21.748 18.972H1.74805L11.748 1.97198Z" fill="#D22837" stroke="#FBE1E9" stroke-width="2"/>
           </svg>
         </button>
         <div class="size-17 bg-crimson rounded-full border-2 border-blush"></div>
-        <button @click="goDown" class="text-crimson">
+        <button @click="goToNextStep" class="text-crimson">
           <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-34 h-auto">
             <path d="M11.748 18L1.74805 1L21.748 1L11.748 18Z" fill="#D22837" stroke="#FBE1E9" stroke-width="2"/>
           </svg>
         </button>
       </div>
+
     </div>
 
     <!-- Content column -->
@@ -133,17 +141,24 @@ watch(() => props.persona, async (newPersona) => {
   }
 }, { immediate: true })
 
-function goUp() {
+function goToPreviousStep() {
   if (activeIndex.value > 0) {
     activeIndex.value--
   }
 }
 
-function goDown() {
+function goToNextStep() {
   if (activeIndex.value < steps.value.length - 1) {
     activeIndex.value++
   }
 }
+
+function goToStep(index) {
+  if (index >= 0 && index < steps.value.length) {
+    activeIndex.value = index
+  }
+}
+
 </script>
 
 <style scoped>
