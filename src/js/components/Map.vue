028297600353e2c366fa4cdd7c33da75da9e53ce
Marcel Stadelmann <m@marceli.to>
2025-06-11 10:39:19 +0200

    wip

--- src/js/components/Map.vue
+++ src/js/components/Map.vue
@@ -1,53 +1,43 @@
-
 <template>
   <figure class="relative">
-    <!-- Dynamic image based on the timeline step -->
-    <img
-      v-if="step?.mapImage"
-      :src="getImageUrl(step.mapImage)"
-      width="1319" height="714"
-      alt=""
-      class="block w-full h-auto"
-    />
+    <!-- Default background image if no step image -->
+    <img 
+      v-if="!props.step?.mapImage"
+      src="/img/map-digitaler-kuhstall.png" 
+      width="1319" height="714" 
+      alt="" 
+      class="block w-full h-auto" 
+      loading="lazy"
+    >
 
-    <!-- Fallback static background image -->
-    <img
+    <!-- Dynamic image if step is available -->
+    <img 
       v-else
-      src="/img/map-digitaler-kuhstall.png"
-      width="1319" height="714"
-      alt=""
-      class="block w-full h-auto"
-    />
+      :src="`/img/${props.step.mapImage}`" 
+      width="1319" height="714" 
+      alt="" 
+      class="block w-full h-auto" 
+      loading="lazy"
+    >
 
-    <!-- Always show language overlay -->
-    <img
-      :src="`/img/map-digitaler-kuhstall-text-${currentLang}.png`"
-      width="1319" height="714"
-      alt=""
+    <!-- Language overlay stays on top -->
+    <img 
+      :src="`/img/map-digitaler-kuhstall-text-${currentLang}.png`" 
+      width="1319" height="714" 
+      alt="" 
       class="block w-full h-auto absolute top-0 left-0"
-    />
+      loading="lazy"
+    >
   </figure>
 </template>
+
 <script setup>
 import { computed } from 'vue'
 import { useLanguageStore } from '@/js/stores/language'
-
 const languageStore = useLanguageStore()
 const currentLang = computed(() => languageStore.current)
 
 const props = defineProps({
   step: Object
 })
-
-// Updated import.meta.glob using the correct options
-const images = import.meta.glob('@/assets/img/map-digitaler-kuhstall-*.png', {
-  eager: true,
-  query: '?url',
-  import: 'default',
-})
-
-// Utility to resolve image URLs from filenames
-function getImageUrl(filename) {
-  return images[`@/assets/img/${filename}`] || ''
-}
 </script>
