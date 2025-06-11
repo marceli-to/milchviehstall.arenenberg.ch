2e9b30d9004cc78ca0f949e491bbeb37f570425a
Marcel Stadelmann <m@marceli.to>
2025-06-11 10:27:24 +0200

    wip

--- src/js/components/Map.vue
+++ src/js/components/Map.vue
@@ -1,43 +1,53 @@
 <template>
   <figure class="relative">
-    <!-- Default background image if no step image -->
-    <img 
-      v-if="!props.step?.mapImage"
-      src="/img/map-digitaler-kuhstall.png" 
-      width="1319" height="714" 
-      alt="" 
-      class="block w-full h-auto" 
-      loading="lazy"
-    >
+    <!-- Show dynamic timeline image if available -->
+    <img
+      v-if="step?.mapImage"
+      :src="getImageUrl(step.mapImage)"
+      width="1319" height="714"
+      alt=""
+      class="block w-full h-auto"
+    />
 
-    <!-- Dynamic image if step is available -->
-    <img 
+    <!-- Fallback image if no step is selected -->
+    <img
       v-else
-      :src="`/img/${props.step.mapImage}`" 
-      width="1319" height="714" 
-      alt="" 
-      class="block w-full h-auto" 
-      loading="lazy"
-    >
+      src="/img/map-digitaler-kuhstall.png"
+      width="1319" height="714"
+      alt=""
+      class="block w-full h-auto"
+    />
 
-    <!-- Language overlay stays on top -->
-    <img 
-      :src="`/img/map-digitaler-kuhstall-text-${currentLang}.png`" 
-      width="1319" height="714" 
-      alt="" 
+    <!-- Language overlay (always shown) -->
+    <img
+      :src="`/img/map-digitaler-kuhstall-text-${currentLang}.png`"
+      width="1319" height="714"
+      alt=""
       class="block w-full h-auto absolute top-0 left-0"
-      loading="lazy"
-    >
+    />
   </figure>
 </template>
 
 <script setup>
 import { computed } from 'vue'
 import { useLanguageStore } from '@/js/stores/language'
+
 const languageStore = useLanguageStore()
 const currentLang = computed(() => languageStore.current)
 
 const props = defineProps({
   step: Object
 })
+
+// Import all possible timeline images and map them to URLs
+const images = import.meta.glob('/img/map-digitaler-kuhstall-*.png', {
+  eager: true,
+  as: 'url'
+})
+
+// Utility function to safely get the correct image URL
+function getImageUrl(filename) {
+  return images[`/img/${filename}`] || ''
+}
 </script>
+
